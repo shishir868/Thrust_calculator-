3e6e9c11e7741bcfcdeed759b2f5bc274c7e1e9d
shishir868 <shishirachar16@gmail.com>
2025-08-10 01:25:39 +0530

    Update

--- trust_calc_.py
+++ trust_calc_.py
@@ -1,12 +1,12 @@
 mfr = int(input("Mass Flow Rate: "))
-print(mfr)
+print(mfr, "kg/s")
 ve = int(input("Exhaust Velocity: "))
-print(ve)
+print(ve, "m/s")
 
 Thrust = mfr*ve
 
 if (Thrust > 0):
-    print(Thrust)
+    print("The required thrust is:", Thrust, "N")
 elif (Thrust <= 0):
     print("INVALID INPUT")
 
